2585c5ef4ecc6ec680833d6d9a02c20effb9c272
Guillaume Blanc de Lanaute <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2019-11-16 17:25:16 +0100

    Création de la classe et variable des posts

--- angular/blog-activity-v1/src/app/app.component.ts
+++ angular/blog-activity-v1/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'blog-activity-v1';
+    posts: Post[] = [
+        new Post('Mon premier post', 'Content premier post', 0, new Date()),
+        new Post('Mon deuxième post', 'Content deuxième post', 0, new Date()),
+        new Post('Encore un post', 'Content dernier post', 0, new Date())
+    ];
 }
